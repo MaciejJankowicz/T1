b176d639e03652ef1ed41482b0da3ecdcb2383c1
student <student@L-412-PC08>
2017-10-02 18:43:11 +0200

    init

--- T1/Program.cs
+++ T1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace T1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Podaj ścieżkę do pliku");
+            string toRead = Console.ReadLine();
+
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader(toRead))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    String line = sr.ReadToEnd();
+                    Console.WriteLine("File opened");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
+            SymbolReader SR = new SymbolReader(toRead);
         }
     }
 }
---
+++ T1/SymbolReader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T1
+{
+    public class SymbolType
+    {
+        public string Type;
+        public string Regex;
+    }
+    public class FoundSymbol
+    {
+        public string content;
+        public SymbolType type;
+    }
+    public class SymbolReader
+    {
+        public static SymbolType[] MatchTypes = new[] {
+            new SymbolType {Type = "identyfikator",Regex = @"[^\W_]+"},
+            new SymbolType {Type = "int",Regex = @"\d+"},
+            new SymbolType {Type = "float",Regex = @"\d+\.\d+"},
+            new SymbolType {Type = "plus",Regex = @"\+"},
+            new SymbolType {Type = "minus",Regex = @"-"},
+            new SymbolType {Type = "gwiazdka",Regex = @"\*"},
+            new SymbolType {Type = "ukośnik",Regex = @"\/"},
+            new SymbolType {Type = "nawiasLewy",Regex = @"\("},
+            new SymbolType {Type = "nawiasPrawy",Regex = @"\)"},
+            new SymbolType {Type = "znBiały",Regex = @"[ \n]+"},
+            new SymbolType {Type = "błąd",Regex = ""},
+        };
+
+        string baseText;
+        string currentText;
+        public SymbolReader(string text)
+        {
+            baseText = text;
+            currentText = text;
+        }
+
+        public FoundSymbol GetNextSymbol()
+        {
+            List<SymbolType> currentSymbols;
+            bool isFinished = false;
+            string currentFragment = "";
+            do
+            {
+                currentSymbols = new List<SymbolType>();
+                //currentFragment += currentText
+
+            }
+            while (!isFinished);
+
+        }
+    }
+}
